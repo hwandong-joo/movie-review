62a6de54ff6e55971cf6c5a476383191c2f6f2df
hwandong-joo <hwandong-joo@godo.co.kr>
2021-01-21 16:51:00 +0900

    add #14 - 테스트케이스 추가

---
+++ src/test/java/org/zerock/mreview/service/MovieServiceTests.java
@@ -0,0 +1,18 @@
+package org.zerock.mreview.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MovieServiceTests {
+
+    @Autowired
+    private MovieService movieService;
+
+    @Test
+    public void testRemove(){
+        Long mno = 107L;
+        movieService.removeWithReplies(mno);
+    }
+}
