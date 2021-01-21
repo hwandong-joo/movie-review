24ed0e7bdbf194520fc52b2249ecbc5300901d3c
hwandong-joo <hwandong-joo@godo.co.kr>
2021-01-21 17:48:40 +0900

    close #14 - 삭제 구현 완료

--- src/main/java/org/zerock/mreview/controller/MovieController.java
+++ src/main/java/org/zerock/mreview/controller/MovieController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.mreview.dto.MovieDTO;
 import org.zerock.mreview.dto.PageRequestDTO;
@@ -44,5 +41,13 @@ public class MovieController {
         log.info("mno: " + mno);
         MovieDTO movieDTO = movieService.getMovie(mno);
         model.addAttribute("dto", movieDTO);
+    }
+
+    @PostMapping("/remove")
+    public String remove(long mno, RedirectAttributes redirectAttributes) {
+        log.info("mno: " + mno);
+        movieService.removeWithReplies(mno);
+        redirectAttributes.addFlashAttribute("msg", mno);
+        return "redirect:/movie/list";
     }
 }
